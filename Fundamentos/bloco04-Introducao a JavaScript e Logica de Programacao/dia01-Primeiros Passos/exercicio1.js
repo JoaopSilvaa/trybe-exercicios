baaf12517a0eaeaf10ddb628cd54353dfe78268e
João Antõnio <joaopsilva.dev@gmail.com>
2021-11-26 18:08:18 -0300

    exercicio1 com operações básicas

---
+++ Fundamentos/bloco04-Introducao a JavaScript e Logica de Programacao/dia01-Primeiros Passos/exercicio1.js
@@ -0,0 +1,19 @@
+const a = 12;
+const b = 4;
+
+let adicao = a + b;
+
+let subtracao = a - b;
+
+let multi = a * b;
+
+let div = a / b;
+
+let mod = a % b;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multi);
+console.log(div);
+console.log(mod);
+
