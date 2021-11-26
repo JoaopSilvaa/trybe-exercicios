dd28f79901f4ae6231ffdf8dced72326d72acf10
João Antõnio <joaopsilva.dev@gmail.com>
2021-11-26 18:16:43 -0300

    adicionando exercicio2 com algumas comparações

---
+++ Fundamentos/bloco04-Introducao a JavaScript e Logica de Programacao/dia01-Primeiros Passos/exercicio2.js
@@ -0,0 +1,30 @@
+const a = 1200;
+const b = 100;
+const c = 200;
+const valor = 0;
+
+/* Comparação de dois valores
+if(a>b){
+    console.log("o maior número é: " + a);
+}else{
+    console.log("o maior número é: " +b);
+} */
+
+/* Comparação de 3 valores
+if(a>b && a>c){
+    console.log("o maior é: " +a);
+}else if(b>a && b>c){
+    console.log("o maior é: " +b);
+}else if(c>a && c>b){
+    console.log("o maior é: "+c);
+} */
+
+/* Testando positivo e negativo 
+if(valor>0){
+    console.log("positive");
+}else if(valor<0){
+    console.log("negative");
+}else{
+    console.log("zero");
+} */
+
