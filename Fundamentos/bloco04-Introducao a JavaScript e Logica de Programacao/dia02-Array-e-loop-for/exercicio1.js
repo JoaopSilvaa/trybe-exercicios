6fb1b2a8ff334a1e323a6b587c9a150dc99f213c
João Antõnio <joaopsilva.dev@gmail.com>
2021-11-29 18:25:05 -0300

    exercicio 1 de array e loop for finalizado

--- Fundamentos/bloco04-Introducao a JavaScript e Logica de Programacao/dia02-Array-e-loop-for/exercicio1.js
+++ Fundamentos/bloco04-Introducao a JavaScript e Logica de Programacao/dia02-Array-e-loop-for/exercicio1.js
@@ -2,12 +2,18 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let media =0;
 let maior = 0;
+let impar =0;
+let menor = numbers[0];
 
 for(let numerosArray of numbers){
     console.log(numerosArray);
     soma = soma + numerosArray;
     if(numerosArray>maior)
         maior = numerosArray;
+    if(numerosArray % 2 != 0)
+        impar += 1;
+    if(numerosArray<menor)
+    menor=numerosArray;
 }
 console.log('-----------------------------------------');
 console.log(soma);
@@ -22,3 +28,19 @@ if(media > 20){
 }
 console.log('-----------------------------------------');
 console.log(maior);
+console.log('-----------------------------------------');
+if(impar>0){
+    console.log("Existem " + impar + " impar(es) no array!");
+}else{
+    console.log("Nenhum valor ímpar encontrado");
+}
+console.log('-----------------------------------------');
+console.log(menor);
+console.log('-----------------------------------------');
+let array = [];
+
+for(let i=1; i<26; i+=1){
+    array.push(i);    
+}
+for(let divDois of array)
+    console.log(divDois/2);
