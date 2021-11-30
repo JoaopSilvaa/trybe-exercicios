d0b3793b43d433ede411d4006106fca0846c5b51
João Antõnio <joaopsilva.dev@gmail.com>
2021-11-30 19:44:28 -0300

    uma parte do exercicio

---
+++ Fundamentos/bloco04-Introducao a JavaScript e Logica de Programacao/dia03-Logica-e-Algoritmos/exercicio.js
@@ -0,0 +1,17 @@
+let n = 3;
+let squad = "";
+
+for(let i = 0; i < n; i+=1){
+    squad += "*";
+}
+
+for(let j=0; j<n; j+=1){
+    console.log(squad);
+}
+
+console.log("-------------------------------------");
+
+for (let i = 0; i < n; i+=1){
+    
+} 
+
