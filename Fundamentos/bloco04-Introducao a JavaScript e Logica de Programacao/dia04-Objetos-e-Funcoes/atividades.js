8484088764d2270045a02e061f0f7d1938b77127
João Antõnio <joaopsilva.dev@gmail.com>
2021-12-01 15:54:52 -0300

    terminando as atividades antes da aula ao vivo

---
+++ Fundamentos/bloco04-Introducao a JavaScript e Logica de Programacao/dia04-Objetos-e-Funcoes/atividades.js
@@ -0,0 +1,53 @@
+// Iniciando objetos e manipulando suas propriedades
+let player = {
+    name: 'Marta',
+    latName: 'Silva',
+    age: 34,
+    medals:{
+        golden:2,
+        silver:3
+    },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+};
+
+console.log('A jogadora ' + player.name + ' ' + player.latName + ' tem ' + player.age + ' anos de idade.');
+
+console.log('A jogadora ' + player.name + ' ' + player.latName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
+
+
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+
+// Utilizando for/in
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+  };
+
+for (let hello in names){
+    console.log('Olá ' + names[hello]);
+}
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+for(let index in car){
+    console.log(index, car[index]);
+}
+
+//Funções
+function adicao(a,b){
+    let soma = a+b;
+    return soma;
+}
+
+function sub(a,b){
+    let menos = a-b;
+    return menos;
+}
+
+console.log(adicao(3,4));
+console.log(sub(8,4));
