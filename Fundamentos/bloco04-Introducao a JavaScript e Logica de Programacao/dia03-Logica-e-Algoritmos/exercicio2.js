8d431cbc1c818804434f2c8213bbdf1f1db6a1eb
João Antõnio <joaopsilva.dev@gmail.com>
2021-12-01 12:54:06 -0300

    exercicios que eu não vi

---
+++ Fundamentos/bloco04-Introducao a JavaScript e Logica de Programacao/dia03-Logica-e-Algoritmos/exercicio2.js
@@ -0,0 +1,18 @@
+//fatorial
+let n = 10;
+
+for(let i = (n-1) ; i > 0; i--)
+    n *= i;
+
+console.log(n);
+
+//inverter palavras
+let word ='tryber';
+let palavra = '';
+
+for(let i= (word.length-1); i >= 0; i--)
+    palavra += word[i];
+
+console.log(palavra);
+
+// Maior e menor palavraa
