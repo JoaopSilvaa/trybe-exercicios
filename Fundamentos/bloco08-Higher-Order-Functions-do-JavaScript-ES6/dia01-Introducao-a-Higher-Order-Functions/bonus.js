b8dc6b7e32b02cd5643d4a2e7c361d335f1f7067
João Antõnio <joaopsilva.dev@gmail.com>
2022-01-18 18:29:32 -0300

    exercicio bonus

--- Fundamentos/bloco08-Higher-Order-Functions-do-JavaScript-ES6/dia01-Introducao-a-Higher-Order-Functions/bonus.js
+++ Fundamentos/bloco08-Higher-Order-Functions-do-JavaScript-ES6/dia01-Introducao-a-Higher-Order-Functions/bonus.js
@@ -19,3 +19,45 @@ const mage = {
   };
   
   const battleMembers = { mage, warrior, dragon };
+
+  const damageDragon = () => Math.round(Math.random() *15) + dragon.strength;
+
+  const damageWarrior = () => Math.round(Math.random() * ((warrior.strength) + warrior.strength * warrior.weaponDmg));
+  
+  const attackMage = () => {
+      const attack = {
+          damageMage:  Math.round(Math.random() * ((mage.intelligence) + mage.intelligence * 2)),
+          manaMage: 15
+      }
+      if (mage.mana < 15) {
+        attack.damageMage = 'Não possui mana suficiente';
+        attackMage.manaMage = 0;
+      }
+      return attack;
+  }
+
+  const gameActions = {
+      turnWarrior : (damageWarrior) => {
+        const damageWarriorInDragon = damageWarrior();
+        dragon.healthPoints -= damageWarriorInDragon;
+        warrior.damage = damageWarriorInDragon
+      },
+      turnMage: (attackMage) => {
+          const damageMageInDragon = attackMage();
+          dragon.healthPoints -= damageMageInDragon.damageMage;
+          mage.damage = damageMageInDragon.damageMage;
+          mage.mana -= damageMageInDragon.manaMage;
+      },
+      turnDragon: (damageDragon) => {
+          const damageDragonInWarrior = damageDragon();
+          const damageDragonInMage = damageDragon();
+          dragon.damage = damageDragonInMage + damageDragonInWarrior;
+          mage.healthPoints -= damageDragonInMage;
+          warrior.healthPoints -= damageDragonInWarrior;
+      },
+      finalRound: () => battleMembers,   
+  };
+  gameActions.turnWarrior(damageWarrior);
+  gameActions.turnMage(attackMage);
+  gameActions.turnDragon(damageDragon);
+  console.log(gameActions.finalRound());
