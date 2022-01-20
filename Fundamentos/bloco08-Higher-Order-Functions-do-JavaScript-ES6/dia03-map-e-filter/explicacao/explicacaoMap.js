c47ae0e3a73389f849e2854b743a2c54ee95f296
João Antõnio <joaopsilva.dev@gmail.com>
2022-01-20 15:59:32 -0300

    explicacoes e exercicios do bloco

---
+++ Fundamentos/bloco08-Higher-Order-Functions-do-JavaScript-ES6/dia03-map-e-filter/explicacao/explicacaoMap.js
@@ -0,0 +1,31 @@
+const persons = [
+    { firstName: 'Maria', lastName: 'Ferreira' },
+    { firstName: 'João', lastName: 'Silva' },
+    { firstName: 'Antonio', lastName: 'Cabral' },
+  ];
+  
+  const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+  
+  console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+
+  console.log('----------------------------------------------------');
+
+  const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+console.log('----------------------------------------------------');
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
