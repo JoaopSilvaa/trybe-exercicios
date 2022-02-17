28af8b27bf2b696ac820c79e6c6f3ca6a48077a4
Joao Antonio <joaopsilva.dev@gmail.com>
2022-02-17 20:20:37 -0300

    exercicios do dia 10.2

---
+++ Front End/dia02-Componentes-React/my-pokedex/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Pokedex from './Pokedex';
+
+function App() {
+  return (
+    <>
+    <h1>Pokedex</h1>
+    <Pokedex />    
+    </>
+  );
+}
+
+export default App;
---
+++ Front End/dia02-Componentes-React/my-pokedex/src/Pokedex.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Pokemon from "./Pokemon";
+
+class Pokedex extends React.Component{
+    render() {
+        return (
+            <div>
+                <Pokemon />
+            </div>
+        )
+    }
+}
+
+
+export default Pokedex
