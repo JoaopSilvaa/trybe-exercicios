146ecbe748829da4bdf011d020ffad6a461701b0
Joao Antonio <joaopsilva.dev@gmail.com>
2022-06-01 12:22:36 -0300

    enviando os exercicios

---
+++ back-end/bloco-21-Funcoes-SQL-JOINS-Normalizacao/dia01-funcoes-mais-usadas-no-sql/atividade/AVG-MIN-MAX-SUM-COUNT.sql
@@ -0,0 +1,9 @@
+SELECT AVG(length) FROM sakila.film;
+
+SELECT MIN(length) FROM sakila.film;
+
+SELECT MAX(length) FROM sakila.film;
+
+SELECT SUM(length) FROM sakila.film;
+
+SELECT COUNT(*) FROM sakila.film;
