0adb1436c22c62511191afe8c8159a481b58382b
Joao Antonio <joaopsilva.dev@gmail.com>
2022-07-08 17:37:18 -0300

    atividade completa

---
+++ back-end/bloco-24-Node-js-ORM-e-Autenticacao/dia02-Associations/exemplos/exemplo5-unmanaged-transactions/index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+
+const { Address, Employee } = require('./models');
+const config = require('./config/config');
+
+const app = express();
+app.use(bodyParser.json());
+
+const sequelize = new Sequelize(config.development);
+
+app.get('/employees', async (_req, res) => {
+  try {
+    const employees = await Employee.findAll({
+      include: { model: Address, as: 'addresses' },
+    });
+
+    return res.status(200).json(employees);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  };
+});
+
+
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findOne({
+        where: { id },
+        include: [{
+          model: Address, as: 'addresses', attributes: { exclude: ['number'] },
+        }],
+      });
+
+    if (!employee)
+      return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+app.post('/employees', async (req, res) => {
+  // Primeiro iniciamos a transação
+  const t = await sequelize.transaction();
+
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transation: t },
+      );
+
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transation: t },
+      );
+
+    // Se chegou até essa linha quer dizer que nenhum erro ocorreu.
+    // Com isso, podemos finalizar a transação usando a função `commit`.
+    await t.commit();
+
+    return res.status(201).json({ message: 'Cadastrado com sucesso' });
+  } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário
+    // fazer manualmente
+
+    await t.rollback();
+
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
+
+module.exports = app;
